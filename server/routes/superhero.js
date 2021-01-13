637fb33cdd7d5ba21eebd9caae42f0aa0a07e42f
Tony Enerson <tony@enerson.ca>
2021-01-12 17:41:49 -0700

    Removed extra endpoint

--- server/routes/superhero.js
+++ server/routes/superhero.js
@@ -11,13 +11,4 @@ router.get('/', async (req, res, next) => {
   res.send(data);
 });
 
-/* Details of a given superhero. */
-router.get('/:name', function(req, res, next) {
-  let name = req.params.name;
-  if (! name) {
-    res.status(400).send('Bad request - `name` missing')
-  }
-  res.send(SUPERHEROES.filter(hero => hero.name === name));
-});
-
 module.exports = router;
